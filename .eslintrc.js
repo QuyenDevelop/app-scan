6b5daebabb41e3294ea4a790ef2e1beffd7dbfe4
huyvv <huyvv@ichiba.vn>
2021-07-26 08:19:50 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  extends: ["@react-native-community"],
+  plugins: ["react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    quotes: [2, "double"],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
