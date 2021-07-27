e60d18bc11f6ab5b7193cfb38282ee84439e9a27
huyvv <huyvv@ichiba.vn>
2021-07-27 13:52:00 +0700

    config api

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -4,26 +4,33 @@ import { BaseApi } from "./base-api";
 
 const {
   IDENTITY_HOST,
-  GRANT_TYPE_PASSWORD,
   GRANT_TYPE_EXTERNAL,
   GRANT_TYPE_REFRESH_TOKEN,
   SCOPES,
   CLIENT_ID,
   CLIENT_SECRET,
+  GRANT_TYPE_PASSWORD,
+  // REDIRECT_URI,
+  // POST_LOGOUT_REDIRECT_URI,
+  // LOAD_USER_INFO,
+  // SILENT_REDIRECT_URI,
+  // AUTOMATIC_SILENT_RENEW,
+  // REVOKE_ACCESS_TOKEN_ON_SIGN_OUT,
+  // RESPONSE_TYPE,
 } = Config;
 
 class AuthApi extends BaseApi {
   login(email?: string, password?: string) {
-    let url = `${IDENTITY_HOST}/connect/token`;
+    let url = `${IDENTITY_HOST}`;
+    console.log("🚀🚀🚀 => login => url", url);
     return this.postUrlEncoded(
       url,
       {
         username: email,
         password: password,
-        grant_type: GRANT_TYPE_PASSWORD,
+        client_id: CLIENT_ID,
         client_secret: CLIENT_SECRET,
         scope: SCOPES,
-        client_id: CLIENT_ID,
       },
       true,
     );
