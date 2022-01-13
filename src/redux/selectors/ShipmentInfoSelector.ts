436c7f1713322dc2e85d07f534c539f39b5fed75
pham hong quyen <quyenph@ichiba.vn>
2022-01-13 17:09:00 +0700

    new modal choose location

--- src/redux/selectors/ShipmentInfoSelector.ts
+++ src/redux/selectors/ShipmentInfoSelector.ts
@@ -1,4 +1,8 @@
-import { CustomerResponse, ShipmentStatusResponse } from "@models";
+import {
+  CustomerResponse,
+  LocationResponse,
+  ShipmentStatusResponse,
+} from "@models";
 import { IRootState } from "@redux";
 import { createSelector } from "reselect";
 
@@ -25,6 +29,19 @@ export const selectorAllStatus = (filterValue: string) => {
         item.Name.trim()
           .toLowerCase()
           .includes(filterValue.trim().toLowerCase()),
+      );
+    },
+  );
+};
+
+export const getLocationReducer = (state: IRootState) =>
+  state.shipmentInfo.shipmentLocations;
+export const selectorLocation = (filterValue: string) => {
+  return createSelector(
+    [getLocationReducer],
+    (Location: Array<LocationResponse>) => {
+      return Location.filter(c =>
+        c.Name.trim().toLowerCase().includes(filterValue.trim().toLowerCase()),
       );
     },
   );
